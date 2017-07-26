b3ae9b2fdea2e8f8988d1a66aeb6d821d3c81d37
Aliou BA <alioubafr@gmail.com>
2017-07-26 10:56:57 +0000

    Install Docker on Centos 7

---
+++ docker/installation/centos7-docker-ce.sh
@@ -0,0 +1,8 @@
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager     --add-repo     https://download.docker.com/linux/centos/docker-ce.repo
+yum-config-manager --disable docker-ce-edge
+yum makecache fast
+yum install docker-ce
+systemctl start docker
+systemctl enable docker
+systemctl status docker
